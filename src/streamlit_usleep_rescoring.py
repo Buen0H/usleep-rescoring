cb796260b474f47a8c5029881dfb9a0ba8d03af8
Buen0H <hbueno@stanford.edu>
2025-04-29 15:41:24 -0700

    added buttons and page configurations

--- src/streamlit_usleep_rescoring.py
+++ src/streamlit_usleep_rescoring.py
@@ -43,17 +43,30 @@ FS_PSG = 500 # get from edf file instead.
 
 def main():
     """Main function to run the Streamlit app."""
-    # Initialize logging
-    logging_init()
     logging.info("Starting Streamlit app.")
-    logging.debug("TEST")
     # Set the title of the app
+    st.set_page_config(
+        page_title = "U-Sleep Rescoring Tool",
+        page_icon = ":sleeping:",
+        layout = "wide",
+        initial_sidebar_state = "expanded",
+        # menu_items = {
+        #     'Get Help': 'https://www.extremelycoolapp.com/help',
+        #     'Report a bug': "https://www.extremelycoolapp.com/bug",
+        #     'About': "# This is a header. This is an *extremely* cool app!"
+        # }
+    )
     st.title("U-Sleep Rescoring Tool")
     # Create mechanism to import files.
-    ret = sidebar_import_data()
-    if ret is None:
-        logging.warning("No files uploaded.")
+    data = sidebar_import_data()
+    # Check if data is None
+    if data is None:
         st.warning("Please upload files to continue with rescoring.")
+    else:
+        # Process uploaded files
+        data["scoring_processed"] = analyze_uncertain_periods(data["scoring"])
+        # Plot the uploaded files
+        fig = draw_figure_init(data)
         # # Process uploaded files
         # uncertain_info = analyze_uncertain_periods(scoring, scoring_time_hrs)
         # naive_scoring = np.argmax(scoring, axis=1)
@@ -66,7 +79,13 @@ def main():
         # # Plot figures
         # fig = draw_figure(data_scoring=naive_scoring, data_biosignals=raw_obj,
         #                 time_slice=visible_time_slice)
-        # st.pyplot(fig)
+        st.pyplot(fig)
+        # Buttons to navigate through the data
+        rewind, back, forward, fast_forward = st.columns(4)
+        rewind.button("Rewind", key="rewind", use_container_width=True)
+        back.button("Back", key="back", use_container_width=True)
+        forward.button("Forward", key="forward", use_container_width=True)
+        fast_forward.button("Fast forward", key="fast_forward", use_container_width=True)
 
 def logging_init():
     """Initialize logging."""
@@ -94,47 +113,50 @@ def sidebar_import_data():
     # Check if files are uploaded
     if uploaded_files == []:
         return None
+    else:
+        logging.info("Files uploaded.")
     # Import uploaded files
-    logging.info("Files uploaded.")
+    unpacked_data = {
+        "raw_obj": None,
+        "scoring": None,
+    }
     for uploaded_file in uploaded_files:
         filename = uploaded_file.name
         if filename.endswith(".npy"):
             logging.debug(f"Found scoring file: {filename}")
-            scoring = np.load(uploaded_file)
-            scoring_time_hrs = np.arange(scoring.shape[0]) * 30 / 3600  # Every data point corresponds to a 30 second epoch; convert to hours.
+            unpacked_data["scoring"] = np.load(uploaded_file)
+            # Every data point corresponds to a 30 second epoch; convert to hours.
+            scoring_time_hrs = np.arange(unpacked_data["scoring"].shape[0]) * 30 / 3600 
             scoring_duration = scoring_time_hrs[-1]
-            st.write(f"Found scoring file labeled {filename} with \
-                    {scoring_duration:.0f} hours and {scoring_duration%1*60:.0f} minutes.")
-            logging.debug(f"Scoring shape: {scoring.shape}")
+            logging.info(f"Loaded scoring file labeled {filename} with " + \
+                    f"{scoring_duration:.0f} hours and {scoring_duration%1*60:.0f} minutes.")
         elif filename.endswith(".edf"):
             logging.info(f"Found raw data file: {filename}")
-            old_import_edf_file(uploaded_file)
+            unpacked_data["raw_obj"] = import_edf_file(uploaded_file)
 
-            # ch_labels = raw_obj.ch_names
-            # _, time_sec = raw_obj[:]
-            # st.write(f"Found PSG file with the following signals: {ch_labels} and a duration of \
-            #         {time_sec[-1]/3600:.0f} hours and {time_sec[-1]/3600%1*60:.0f} minutes.")
-            logging.info(f"Old Raw data loaded.")
+            ch_labels = unpacked_data["raw_obj"].ch_names
+            _, time_sec = unpacked_data["raw_obj"][:]
+            logging.info(f"Loaded PSG file with the following signals: {ch_labels} and a duration of " + \
+                    f"{time_sec[-1]/3600:.0f} hours and {time_sec[-1]/3600%1*60:.0f} minutes.")
+    # Check if both files are uploaded
+    if unpacked_data is None:
+        logging.warning("No files uploaded.")
+        st.sidebar.warning("Please upload files to continue with rescoring.")
+        return None
+    elif unpacked_data["raw_obj"] is None:
+        logging.warning("No raw data uploaded.")
+        st.sidebar.warning("Please upload PSG data.")
+        return None
+    elif unpacked_data["scoring"] is None:
+        logging.warning("No scoring data uploaded.")
+        st.sidebar.warning("Please scoring file.")
+        return None
+    else:
+        # Display data
+        st.sidebar.success("Files uploaded successfully.")
+        return unpacked_data
 
-            logging.info("Trying new import method.")
-            import_edf_file(uploaded_file)
-            logging.info("New Raw data loaded.")
-    return []
-    #     # Process uploaded files
-    #     uncertain_info = analyze_uncertain_periods(scoring, scoring_time_hrs)
-    #     naive_scoring = np.argmax(scoring, axis=1)
-    #     # Look through uncertain periods
-    #     for uncertain_period in uncertain_info["uncertain_periods"]:
-    #         slice_start = int(uncertain_period["start_hour"]*3600)*FS_PSG
-    #         slice_stop = int(slice_start + 30)*FS_PSG
-    #         visible_time_slice = np.s_[slice_start:slice_stop]
-    #         break
-    #     # Plot figures
-    #     fig = draw_figure(data_scoring=naive_scoring, data_biosignals=raw_obj,
-    #                     time_slice=visible_time_slice)
-    #     st.pyplot(fig)
-
-def old_import_edf_file(uploaded_file):
+def import_edf_file(uploaded_file):
     """Import EDF file."""
     # Create a temporary file
     with tempfile.NamedTemporaryFile(delete=False, suffix=".edf") as temp_file:
@@ -143,25 +165,13 @@ def old_import_edf_file(uploaded_file):
 
     # Read the EDF file using MNE
     raw_obj = mne.io.read_raw_edf(temp_file_path, preload=True)
+    return raw_obj
 
-def import_edf_file(uploaded_file):
-    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-        tmp_filename = tmp_file.name
-        uploaded_file.seek(0)
-        tmp_file.write(uploaded_file.read())
-
-        edf_data = pyedflib.EdfReader(tmp_filename)
-        num_channels = edf_data.signals_in_file
-        channel_names = edf_data.getSignalLabels()
-        raw_data = []
-        min_length = float('inf')  # Initialize minimum length with a large value
-        for channel in range(num_channels):
-            channel_data = edf_data.readSignal(channel)
-            raw_data.append(channel_data)
-            min_length = min(min_length, len(channel_data))
-
-def analyze_uncertain_periods(confidence_data: np.ndarray, time_hrs: np.ndarray) -> Dict:
+def analyze_uncertain_periods(confidence_data: np.ndarray) -> Dict:
     """Analyze periods of low confidence."""
+    # Get the time in hours
+    time_hrs = np.arange(confidence_data.shape[0]) * 30 / 3600  # 30 seconds per epoch
+
     max_confidences = np.max(confidence_data, axis=1)
     max_possible_conf = np.ceil(np.max(max_confidences))  # Round up to nearest whole number
     relative_threshold = 0.67  # equivalent to 4/6
@@ -197,16 +207,18 @@ def analyze_uncertain_periods(confidence_data: np.ndarray, time_hrs: np.ndarray)
         'uncertain_periods': uncertain_periods,
         'total_uncertain_mins': sum(p['duration_mins'] for p in uncertain_periods),
         'confidence_threshold': confidence_threshold,
-        'max_possible_conf': max_possible_conf
+        'max_possible_conf': max_possible_conf,
+        "scoring_naive": np.argmax(confidence_data, axis=1),
+        "time_hrs": time_hrs,  # 30 seconds per epoch
     }
 
-def draw_figure(data_scoring, data_biosignals, time_slice, period_scoring=30):
+def draw_figure_init(data_dict: Dict):
     '''
     INPUTS
     period_scoring (int) - scoring sampling period in seconds.
     '''
     # Create the figure and GridSpec layout
-    fig = plt.figure(figsize=(6, 6))
+    fig = plt.figure(figsize=(18, 7), dpi=300)
     gs = gridspec.GridSpec(2, 1, height_ratios=[1, 5])  # 1 unit for top, 5 for bottom
 
     # Create the top and bottom axes
@@ -214,36 +226,50 @@ def draw_figure(data_scoring, data_biosignals, time_slice, period_scoring=30):
     ax_bottom = fig.add_subplot(gs[1])
 
     # Top plot. Algorithm scoring output.
-    time_scoring = np.arange(len(data_scoring))*period_scoring/3600
-    ax_top.plot(time_scoring, data_scoring, linewidth=0.5, color="grey")
-    ax_top.fill_betweenx(y=[0, 5], x1=time_slice.start/3600/30, x2=time_slice.stop/3600/30, color="red", alpha=0.3)
+    scoring_data = data_dict["scoring_processed"]
+    scoring_naive = scoring_data["scoring_naive"]
+    time = scoring_data["time_hrs"]
+    ax_top.plot(time, scoring_naive, linewidth=1, color="grey")
+    # Highlight uncertain periods.
+    uncertain_periods = scoring_data["uncertain_periods"]
+    for uncertain_period in uncertain_periods:
+        start_hour = uncertain_period["start_hour"]
+        end_hour = uncertain_period["end_hour"] + 30 / 3600
+        ax_top.fill_betweenx(y=[0, 5], x1=start_hour, x2=end_hour, color="red", alpha=0.3)
+    
     ax_top.set_yticks(ticks=range(5), labels=SLEEP_STAGE_LABELS)
     ax_top.set_ylim(4.25,-0.25)
     ax_top.set_xlabel("Time (hrs)")
     ax_top.set_ylabel("Sleep stages")
 
-    # Bottom plot. Raw data display.
-    # data_biosignals = get_sorted_biosignals(data_biosignals)
-    # signals, time = data_biosignals[:]
-    # ch_labels = data_biosignals.ch_names
-    signals, time, ch_labels = get_sorted_biosignals(data_biosignals)
-    for idx, signal in enumerate(signals[:,time_slice]):
-        # autoscaling 
-        if SELECT_AUTOSCALING == "MINMAX":
-            c_minmax = signal.max() - signal.min()
-            signal /= c_minmax
-        elif SELECT_AUTOSCALING == "RMS":
-            c_rms = np.sqrt(np.mean(signal**2))
-            signal /= c_rms
-        ax_bottom.plot(time[time_slice], signal + idx, linewidth=0.75)  # Blue diagonal line
-    ax_bottom.set_yticks(range(signals.shape[0]), ch_labels);
-    ax_bottom.set_ylim(signals.shape[0], -1)
-    ax_bottom.set_xlabel("Time (s)")
+    # # Bottom plot. Raw data display.
+    # # data_biosignals = get_sorted_biosignals(data_biosignals)
+    # # signals, time = data_biosignals[:]
+    # # ch_labels = data_biosignals.ch_names
+    # signals, time, ch_labels = get_sorted_biosignals(data_biosignals)
+    # for idx, signal in enumerate(signals[:,time_slice]):
+    #     # autoscaling 
+    #     if SELECT_AUTOSCALING == "MINMAX":
+    #         c_minmax = signal.max() - signal.min()
+    #         signal /= c_minmax
+    #     elif SELECT_AUTOSCALING == "RMS":
+    #         c_rms = np.sqrt(np.mean(signal**2))
+    #         signal /= c_rms
+    #     ax_bottom.plot(time[time_slice], signal + idx, linewidth=0.75)  # Blue diagonal line
+    # ax_bottom.set_yticks(range(signals.shape[0]), ch_labels);
+    # ax_bottom.set_ylim(signals.shape[0], -1)
+    # ax_bottom.set_xlabel("Time (s)")
 
     # Adjust layout
     plt.tight_layout()
     
     return fig
+
+def draw_figure_update(fig, slice_window):
+    axs = fig.get_axes()
+    ax_top = axs[0]
+    ax_top.fill_betweenx(y=[0, 5], x1=slice_window.start/3600/30, x2=slice_window.stop/3600/30, color="red", alpha=0.3)
+    st.pyplot(fig)
 
 def get_sorted_biosignals(mne_raw_obj):
     ''' Reorder channels to EOG x2, front EEG, ... , back EEG, EMG x2 '''
@@ -273,6 +299,9 @@ def get_sorted_biosignals(mne_raw_obj):
     return signals, time, ch_labels
 
 if __name__ == "__main__":
+    # Initialize logging
+    logging_init()
+    # Run the main function
     main()
 
 
